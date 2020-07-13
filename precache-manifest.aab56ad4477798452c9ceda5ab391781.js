438dc4e29d1aa1def52c7e5e13aba6fa2d5e6658
Kunal Sarpatil <ksarpatil7@gmail.com>
2020-07-13 09:20:22 +0530

    Updates

---
+++ precache-manifest.aab56ad4477798452c9ceda5ab391781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7671af8fe81b656dfaed01f69be1d3",
+    "url": "/wilson/index.html"
+  },
+  {
+    "revision": "eb9640605ec2d33cedbb",
+    "url": "/wilson/static/css/main.bcb88a18.chunk.css"
+  },
+  {
+    "revision": "5532a41b71b220d2ee34",
+    "url": "/wilson/static/js/2.117cfb28.chunk.js"
+  },
+  {
+    "revision": "a015ad1c06863e995b139f76b104aa05",
+    "url": "/wilson/static/js/2.117cfb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9640605ec2d33cedbb",
+    "url": "/wilson/static/js/main.c90162b6.chunk.js"
+  },
+  {
+    "revision": "064ebd8a242095a03438",
+    "url": "/wilson/static/js/runtime-main.0722fabd.js"
+  }
+]);
